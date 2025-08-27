b8e8d20ec9a68b946c763f338ca3c23dd59a5c0f
Jessie King <jessie.kinganga@gmail.com>
2025-08-27 14:55:00 +0100

    Integrated HTML and CSS to build the frontend design.

--- challenge/app.py
+++ challenge/app.py
@@ -1,23 +1,22 @@
-from flask import Flask
+
+from flask import Flask, render_template
 import redis
 import os
 
-
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 REDIS_HOST = os.getenv('REDIS_HOST', 'redis')
 REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
 redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
 
-
 @app.route('/')
 def welcome():
-    return 'Welcome to the Coderco Visitor Counter Challenge'
- 
-@app.route('/count')   
-def count_visitor():   
+    return render_template('index.html')
+
+@app.route('/count')
+def count_visitor():
     visitor_count = redis_client.incr('visitor_count')
-    return f'This page has been visited {visitor_count} times'
+    return render_template('count.html', count=visitor_count)
 
 
 if __name__ == '__main__':
